4c7d3d539a2fc12b2b16f4b60ec5bcdc35342393
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2021-05-29 16:53:52 +0100

    Activity 1 EF database first Exemple

--- TP2_Activity1/DBA_EF_DataBaseFirst/Program.cs
+++ TP2_Activity1/DBA_EF_DataBaseFirst/Program.cs
@@ -10,6 +10,38 @@ namespace DBA_EF_DataBaseFirst
     {
         static void Main(string[] args)
         {
+            StudentRepository repository = new StudentRepository();
+
+            Student student = new Student();
+            student.Name = "Ramzi";
+            student.LastName = "Latrous";
+            student.email = "ramzi.latrous@insat.u-carthage.tn";
+            student.Age = 22;
+
+            repository.Add(student);
+
+            List<Student> students = repository.FindAll();
+
+            foreach(Student looper in students)
+            {
+                Console.WriteLine(student.Name);
+            }
+
+            Student s1 = students.FirstOrDefault();
+            Student s2 = students[1];
+            
+            s1.Age = 50;
+            repository.Update(s1);
+
+
+            repository.Delete(s2.Id);
+
+            Console.WriteLine(repository.FindById(s1.Id).Age);
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
---
+++ TP2_Activity1/DBA_EF_DataBaseFirst/StudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBA_EF_DataBaseFirst
+{
+    class StudentRepository
+    {
+        private Context context;
+
+        public StudentRepository()
+        {
+            context = new Context();
+        }
+
+        public void Add (Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+        public void Delete (int id)
+        {
+            Student student = context.Students.SingleOrDefault(s => s.Id == id);
+            context.Students.Remove(student);
+            context.SaveChanges();
+        }
+
+        public void Update(Student student)
+        {
+            Student updated = context.Students.SingleOrDefault(s => s.Id == student.Id);
+            updated.Name = student.Name;
+            updated.LastName = student.LastName;
+            updated.email = student.email;
+            updated.Age = student.Age;
+            context.SaveChanges();
+        }
+
+        public Student FindById(int id)
+        {
+            return context.Students.SingleOrDefault(s => s.Id == id);
+        }
+
+        public List<Student> FindAll()
+        {
+            return context.Students.ToList();
+        }
+    }
+}
